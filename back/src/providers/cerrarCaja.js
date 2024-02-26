03195df02e0bf7f094324d013d265fe33c2a6e10
Jeremias Barolo <barolojeremias@gmail.com>
2024-02-26 15:46:08 -0300

    cerrar caja terminado
    cerrar caja terminado

--- back/src/providers/cerrarCaja.js
+++ back/src/providers/cerrarCaja.js
@@ -1,14 +1,27 @@
 
 var models = require('../models');
-
+var models = require('../models');
+const fs = require('fs');
+const path = require('path');
+const puppeteer = require('puppeteer');
 const {listAllmaestro_articulos, listOnemaestro_articulos} = require('./maestro_articulos');
-const {listAllventa} = require('./venta');
+
+// <=================== fecha de realizacion =================>
+const now = new Date();
+const year = now.getFullYear();
+const month = String(now.getMonth() + 1).padStart(2, '0');
+const day = String(now.getDate()).padStart(2, '0');
+const hours = String(now.getHours()).padStart(2, '0');
+const minutes = String(now.getMinutes()).padStart(2, '0');
+const seconds = String(now.getSeconds()).padStart(2, '0');
+
+
 
 
 
 const cerrarCaja = async () => {
     try {
-        // const maestros = await listAllmaestro_articulos();
+        const maestros = await listAllmaestro_articulos();
         // const mesas = await listAllventa();
         const mesas = await models.venta.findAll({
                 include: [
@@ -80,6 +93,33 @@ const cerrarCaja = async () => {
 
 // // <========================= Final Ganancia =============================>
 
+
+
+
+
+// <================ Creacion de Estadistica ===========================>
+// await models.estadistica.create({
+//     recaudacion: recaudacionTotal,
+//     costo_total: costoTotal,
+//     profit: ganacia,
+// })
+
+
+// ACA VA LA INTEGRACION DEL PDF
+generarPdf(maestros, recaudacionTotal, costoTotal, ganacia)
+
+// <=================== Eliminar Pedidos ===========================>
+// await models.pedido_produccion.destroy({
+//     where: { estado: 'FINALIZADO' }
+// });
+
+// await models.venta.destroy({
+//     where: { estado: 'FINALIZADO' }
+// });
+
+
+
+
 return {recaudacion: recaudacionTotal, costo: costoTotal, ganancia: ganacia}
         
     } catch (error) {
@@ -130,7 +170,122 @@ const calcularCosto = async (maestros) => {
 }
 
 
+const generarPdf = async (maestro, subtotal,costo_total, ganancia) => {
+
+    const generarPdf = async (maestro) => {
+        const templatePath = path.join(__dirname, '../assets/index.html');
+        const templateContent = fs.readFileSync(templatePath, 'utf-8');
+
+        const documentosPath = path.join(__dirname, `../../../documentos/cajaCerrada_${day}_${month}_${year}_${hours}_${minutes}_${seconds}.pdf`)
+        let fecha = `${day}/${month}/${year} : ${hours}:${minutes} hs`
+
+        
+
+
+
+        let mestro_articulos = maestro.filter((item) => item.tipo_articulo.description !== 'Insumos')
+
+        let data = [];
+
+        for (const maestro of mestro_articulos) {
+            let costoTotal = 0;
+            let cantidadTotal = 0;
+    
+            // Verificar el tipo de artículo
+            if (maestro.tipo_articulo.description === 'Comidas') {
+                // Calcular el costo total para Comidas
+                for (const pedido of maestro.pedido_produccions) {
+                    cantidadTotal += pedido.cant_requerida;
+                    costoTotal += pedido.cant_requerida * maestro.costo_unitario;
+                }
+            } else {
+                // Calcular el costo total para Productos Elaborados y Bebidas
+                for (const receta of maestro.receta) {
+                    const disponibilidadArticulo = receta.disponibilidad_articulo;
+                    const costoUnitarioArticulo = disponibilidadArticulo.maestro_articulo.costo_unitario;
+                    const cantidadNecesaria = receta.cant_necesaria;
+    
+                    // Calcular el costo total del artículo en la receta
+                    costoTotal += costoUnitarioArticulo * cantidadNecesaria;
+                }
+    
+                // Sumar la cantidad total usada
+                for (const pedido of maestro.pedido_produccions) {
+                    cantidadTotal += pedido.cant_requerida;
+                }
+
+                costoTotal = costoTotal*cantidadTotal
+            }
+    
+            // Crear el objeto con la descripción del artículo, cantidad usada y costo unitario
+            const item = {
+                descripcion: maestro.descripcion,
+                cantidadUsada: cantidadTotal,
+                uni_medida: `${maestro.conversion_UM.uni_medida} - ${maestro.conversion_UM.seg_umedida}`,
+                costoUnitario: maestro.costo_unitario,
+                costoTotal: costoTotal,
+                Subtotal: maestro.costo_unitario*cantidadTotal
+            };
+    
+            // Agregar el objeto a la lista externa
+            data.push(item);
+        }
+    
+        
+        console.log(data);
+    
+        const productosHTML = data.map((item) => {
+                return `
+                    <tr>
+                        <td>${item.descripcion}</td>
+                        <td>${item.cantidadUsada}</td>
+                        <td>${item.uni_medida}</td>
+                        <td>$${item.costoUnitario}</td>
+                        <td>$${item.costoTotal}</td>
+                        <td>$${item.Subtotal}</td>
+                        
+                    </tr>
+                `;
+            }).join('');
+
+
+        
+        
+        
+        const facturaHTML = templateContent
+            .replace('{{productosHTML}}', productosHTML)
+            .replace( '{{subtotalTotal}}', subtotal )
+            .replace( '{{costoTotal}}', costo_total)
+            .replace( '{{Profit}}', ganancia)
+            .replace( '{{Fecha}}', fecha)
+
+
+    
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        
+        
+        await page.setContent(facturaHTML);
+    
+        
+        
+        await page.pdf({ path: documentosPath, format: 'A4' });
+    
+        await browser.close();
+    
+        return console.log('PDF generado');
+    };
+    
+    
+    generarPdf(maestro).then((pdfPath) => {
+        console.log(`PDF generado en: ${pdfPath}`);
+    }).catch((error) => {
+        console.error(`Error al generar el PDF: ${error}`);
+    });
+}
+
+
 
 module.exports = {
-cerrarCaja, calcularCosto
+cerrarCaja, calcularCosto, generarPdf
 };
